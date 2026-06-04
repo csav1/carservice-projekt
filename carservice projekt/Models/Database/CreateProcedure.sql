fcede29774d313bf1888f5b125370071aa0504a6
Cristian <cristian.sava@htl.moedling.at>
2026-06-04 13:44:02 +0200

    SQL View und Stored Procedure hinzugefügt

---
+++ carservice projekt/Models/Database/CreateProcedure.sql
@@ -0,0 +1,9 @@
+﻿create procedure sp_WartungenNachStatus
+    @Status varchar(20)
+as 
+begin
+    select *
+    from Wartungen
+    where status = @Status;
+end;
+go
---
+++ carservice projekt/Models/Database/CreateView.sql
@@ -0,0 +1,20 @@
+﻿create view vw_wartungsübersicht
+as
+select
+    f.Marke,
+    f.Modell,
+    s.Bezeichnung as Serviceart,
+
+    w.Datum,
+    w.Kosten,
+    w.Status
+
+from Wartungen w
+
+inner join Fahrzeuge f
+
+    on w.FahrzeugID = f.FahrzeugID
+
+inner join Servicearten s
+    on w.ServiceartID = s.ServiceartID;
+go
